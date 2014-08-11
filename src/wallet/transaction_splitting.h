0f57cd9b5d26ab0895de2fe689c7f852c5cdb740
Zachary Michaels <mikezackles@gmail.com>
2014-08-11 17:38:27 -0400

    Indent and includes

--- src/wallet/transaction_splitting.h
+++ src/wallet/transaction_splitting.h
@@ -1,20 +1,35 @@
 #pragma once
 
+#include "cryptonote_core/cryptonote_format_utils.h"
+
+#include <functional>
+#include <vector>
+
 namespace transaction_splitting
 {
-  void digit_split_strategy(
-      const std::vector<cryptonote::tx_destination_entry>& dsts
-    , const cryptonote::tx_destination_entry& change_dst
-    , uint64_t dust_threshold
-    , std::vector<cryptonote::tx_destination_entry>& splitted_dsts
-    , uint64_t& dust
-    );
 
-  void null_split_strategy(
-      const std::vector<cryptonote::tx_destination_entry>& dsts
-    , const cryptonote::tx_destination_entry& change_dst
-    , uint64_t dust_threshold
-    , std::vector<cryptonote::tx_destination_entry>& splitted_dsts
-    , uint64_t& dust
-    );
+typedef std::function<void (
+    const std::vector<cryptonote::tx_destination_entry>& dsts
+  , const cryptonote::tx_destination_entry& change_dst
+  , uint64_t dust_threshold
+  , std::vector<cryptonote::tx_destination_entry>& splitted_dsts
+  , uint64_t& dust
+  )> strategy;
+
+void digit_split_strategy(
+    const std::vector<cryptonote::tx_destination_entry>& dsts
+  , const cryptonote::tx_destination_entry& change_dst
+  , uint64_t dust_threshold
+  , std::vector<cryptonote::tx_destination_entry>& splitted_dsts
+  , uint64_t& dust
+  );
+
+void null_split_strategy(
+    const std::vector<cryptonote::tx_destination_entry>& dsts
+  , const cryptonote::tx_destination_entry& change_dst
+  , uint64_t dust_threshold
+  , std::vector<cryptonote::tx_destination_entry>& splitted_dsts
+  , uint64_t& dust
+  );
+
 } // namespace transaction_splitting
