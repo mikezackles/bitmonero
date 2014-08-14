9d26a4d53bdb400e865095e9493340c8f0a127a7
Zachary Michaels <mikezackles@gmail.com>
2014-08-14 12:56:50 -0400

    Use raw function pointers for tx splitting strategies

--- src/wallet/transaction_splitting.h
+++ src/wallet/transaction_splitting.h
@@ -2,19 +2,18 @@
 
 #include "cryptonote_core/cryptonote_format_utils.h"
 
-#include <functional>
 #include <vector>
 
 namespace transaction_splitting
 {
 
-typedef std::function<void (
+typedef void (*strategy)(
     const std::vector<cryptonote::tx_destination_entry>& dsts
   , const cryptonote::tx_destination_entry& change_dst
   , uint64_t dust_threshold
   , std::vector<cryptonote::tx_destination_entry>& splitted_dsts
   , uint64_t& dust
-  )> strategy;
+  );
 
 void digit_split_strategy(
     const std::vector<cryptonote::tx_destination_entry>& dsts
