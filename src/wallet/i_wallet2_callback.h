5cc15df62ad602cf0b600fabd5b5ecebf9ec631c
Zachary Michaels <mikezackles@gmail.com>
2014-08-14 12:50:17 -0400

    Clean up wallet interface

--- src/wallet/i_wallet2_callback.h
+++ src/wallet/i_wallet2_callback.h
@@ -11,29 +11,28 @@ public:
   virtual void on_new_block(
       uint64_t height
     , const cryptonote::block& block
-    )
-  {}
+    ) = 0;
 
   virtual void on_money_received(
       uint64_t height
     , const cryptonote::transaction& tx
     , size_t out_index
-    )
-  {}
+    ) = 0;
 
   virtual void on_money_spent(
       uint64_t height
     , const cryptonote::transaction& in_tx
     , size_t out_index
     , const cryptonote::transaction& spend_tx
-    )
-  {}
+    ) = 0;
 
   virtual void on_skip_transaction(
       uint64_t height
     , const cryptonote::transaction& tx
-    )
-  {}
+    ) = 0;
+
+protected:
+  ~i_wallet2_callback() {}
 };
 
 } // namespace tools
