9754f4c6416445fb44f68fb4b1dbf2d5da4b1fcb
Zachary Michaels <mikezackles@gmail.com>
2014-09-04 17:44:50 -0400

    Explicitly check size of tx hash vector in historical fix

--- src/cryptonote_core/hardcoded_historical_tx_fixes.h
+++ src/cryptonote_core/hardcoded_historical_tx_fixes.h
@@ -16,6 +16,13 @@ namespace cryptonote
     case 202612:
       // These transactions may have the wrong hash because of a historical bug
       // in tree_hash.  Here we explicitly use the correct hash.
+
+      if (tx_hashes.size() != 515)
+      {
+        LOG_ERROR("Expected block at height 202612 to contain 515 tx hashes");
+        return;
+      }
+
       epee::string_tools::hex_to_pod(
           "d2d714c86291781bb86df24404754df7d9811025f659c34d3c67af3634b79da6"
         , tx_hashes[513]
@@ -24,6 +31,7 @@ namespace cryptonote
           "d59297784bfea414885d710918c1b91bce0568550cd1538311dd3f2c71edf570"
         , tx_hashes[514]
         );
+
       break;
     }
   }
